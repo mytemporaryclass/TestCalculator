31667e4ac85baf82da049f789e2c3f7b13e6c28c
cylnclk <ceylan3313@homail.com>
2016-05-16 17:06:41 +0300

    İslemler clası eklendi

---
+++ Islemler.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BilgiKontrolLib;
+
+namespace TestCalculater
+{
+    class Islemler
+    {
+        public int  Cikarma(int sayi1,int sayi2)
+        {
+        	return sayi1-sayi2
+        }
+
+        public int carpma(int sayi1,int sayi2)
+        {
+        	return sayi1*sayi2
+        } 
+    }
+}
